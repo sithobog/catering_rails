182a7bbd679929341066341b3dc5ff6c79f8152d
Istomin Bohdan <sithobog@ymail.com>
2015-11-08 21:26:12 +0200

    fix daily_rations JSON and now it removes empty categories

--- app/api/api/v1/resources/daily_rations.rb
+++ app/api/api/v1/resources/daily_rations.rb
@@ -9,19 +9,18 @@ module API
 				desc "Return dishes from daily_menu sorted by category"
 				get do
 					_menus = DailyMenu.all.as_json(except: [:created_at, :updated_at])
-					_categories = Category.all.as_json(except: [:created_at, :updated_at])
 
 					_menus.each do |menu|
-
+						_categories = Category.all.as_json(except: [:created_at, :updated_at])
 						_dishes = Dish.find(menu['dish_ids'])
 						_grouped_dishes = _dishes.group_by {|x| x['category_id']}
-
 						menu['categories'] = _categories
 
 						menu['categories'].each do |category|
 							category['dishes'] = _grouped_dishes[category['id']].as_json(except: [:sort_order, :created_at, :updated_at])
 						end
-
+						#delete categories from JSON if it hasn't dishes
+						menu['categories'].reject! { |cat| !cat['dishes'] }
 						menu.delete('dish_ids')
 
 					end
