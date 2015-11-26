a3291c2548dd25d0bf6d14609d276716285e8743
Istomin Bohdan <sithobog@ymail.com>
2015-11-26 12:33:42 +0200

    add price validation

--- app/api/api/v1/resources/daily_rations.rb
+++ app/api/api/v1/resources/daily_rations.rb
@@ -8,35 +8,64 @@ module API
 			resource :daily_rations do
 
 			  post do
+			  	#find user
 			  	token = request.headers['X-Auth-Token']
 			  	user = User.find_by(authentication_token: token)
-			  	sprint_id = params[:sprint_id]
+			  	#create array for saving in DB
 			  	ar_array = Array.new
-			  	params[:days].each do |param|
-			  		daily_menu_id = param[0]
-			  		param[1].each do |p|
-			  			p[1].each do |k,v|
-			  				ar_array << {
-				  				sprint_id: sprint_id,
-				  				daily_menu_id: daily_menu_id,
-				  				dish_id: k,
-				  				quantity: v
-			  				}
-			  			end
-			  		end
+			  	params.each do |k,v|
+			  		ar_array << {
+			  			sprint_id: v[:sprint_id],
+				  		daily_menu_id: v[:day_id],
+				  		dish_id: v[:dish_id],
+				  		quantity: v[:quantity],
+				  		price: v[:price],
+			  		}
 			  	end
-			  	dish_ids = Array.new
-			  	quantities = Array.new
-			  	ar_array.each do |element|
-			  		dish_ids << element[:dish_id]
-			  		quantities << element[:quantity]
+
+			  	all_days = ar_array.map{|x| x[:daily_menu_id]}
+			  	puts "all days are"
+			  	puts all_days
+			  	puts "unique days are"
+			  	puts all_days.uniq
+			  	all_dishes = Array.new
+			  	ar_array.map do |x|
+				  	all_dishes << { 
+					  	id: x[:dish_id].to_i,
+					  	price: x[:price].to_f
+				  	}
+				  end
+				  #remove duplicates
+			  	uniq_dishes = all_dishes.uniq {|h| h[:id]}
+			  	#sort array
+			  	sorted_uniq_dishes = uniq_dishes.sort_by {|hsh| hsh[:id]}
+			  	#grab all unique dish_id
+			  	all_dishes_id = ar_array.map{|x| x[:dish_id]}.uniq
+			  	#grab only ids and prices of dishes
+			  	dishes = Dish.where(id: all_dishes_id).pluck(:id,:price)
+			  	#create array for comparison
+			  	dishes_from_db = Array.new
+			  	dishes.each do |elem|
+			  		dishes_from_db <<{
+			  			# zero element of array is id, first element is price
+			  			id: elem[0],
+			  			price: elem[1]
+			  		}
 			  	end
-			  	puts dish_ids
-			  	puts quantities
+			  	puts "dishes_from_db is "
+			  	puts dishes_from_db
+			  	puts "sorted uniq dishes"
+			  	puts sorted_uniq_dishes
+			  	#comparison of arrays, if true then prices from client are correct
+			  	if dishes_from_db == sorted_uniq_dishes
+			  		puts "All is good"
+			  	else
+			  		puts "Prices from client are incorrect"
+			  	end
 
 					ActiveRecord::Base.transaction do
 						ar_array.each do |array|
-				  		DailyRation.create(price: 10, quantity: array[:quantity], user_id: user.id,
+				  		DailyRation.create(price: array[:price], quantity: array[:quantity], user_id: user.id,
 				  												daily_menu_id: array[:daily_menu_id], sprint_id: array[:sprint_id], dish_id: array[:dish_id])
 				  	end
 					end
