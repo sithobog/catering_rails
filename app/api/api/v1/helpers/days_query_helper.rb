2e482c377cb945628a872427831271d0be1701a4
Istomin Bohdan <sithobog@ymail.com>
2015-11-29 10:47:10 +0200

    add support for business lunches

--- app/api/api/v1/helpers/days_query_helper.rb
+++ app/api/api/v1/helpers/days_query_helper.rb
@@ -15,12 +15,31 @@ module API
 				_menus.each do |menu|
 					_categories = _categories_from_query.as_json(except: [:created_at, :updated_at])
 					_dishes = Dish.find(menu['dish_ids'])
+
 					_grouped_dishes = _dishes.group_by {|x| x['category_id']}
+
+					_grouped_dishes = _grouped_dishes.as_json(except: [:sort_order, :created_at, :updated_at])
+
+					#add dishes title for children_ids
+					_grouped_dishes.each do |key,value|
+						value.each do |dish|
+							if !dish["children_ids"].nil?
+								child_dishes = Dish.find(dish["children_ids"]).as_json(only: [:id, :title])
+								dish["children"] = child_dishes
+								dish.delete("children_ids")
+								dish["type"] = "BusinessLunch"
+							else
+								dish["type"] = "SingleMeal"
+							end
+						end
+					end
+
 					menu['categories'] = _categories
 
 					menu['categories'].each do |category|
-						category['dishes'] = _grouped_dishes[category['id']].as_json(except: [:sort_order, :created_at, :updated_at])
+						category['dishes'] = _grouped_dishes[category['id'].to_s]
 					end
+
 					#delete categories from JSON if it hasn't dishes
 					menu['categories'].reject! { |cat| !cat['dishes'] }
 					menu.delete('dish_ids')
--- app/api/api/v1/resources/orders.rb
+++ app/api/api/v1/resources/orders.rb
@@ -26,15 +26,24 @@ module API
           puts sprint
           user = User.find_by(authentication_token: token)
 
-          rations = DailyRation.includes(:dish,:daily_menu).where(user_id: user.id, sprint_id: sprint)#.select(:id,:price,:quantity,:daily_menu_id,:dish_id).group_by {|elem| elem[:daily_menu_id]}
+          rations = DailyRation.includes(:dish,:daily_menu).where(user_id: user.id, sprint_id: sprint)
+
           puts "rations is "
           new_rations = Array.new
           rations.each do |ration|
             daily_menu = { day_number: ration.daily_menu.day_number}
+
+            if ration.dish.type == "BusinessLunch"
+              children = Dish.find(ration.dish.children_ids).as_json(only: [:id, :title])
+            end
+            dish_children = {children: children}
+
             dish = { title: ration.dish.title, description: ration.dish.description}
             ration = ration.as_json(except: [:id, :user_id, :daily_menu_id, :sprint_id, :created_at, :updated_at,:dish_id])
 
-            new_rations << ration.merge(daily_menu).merge(dish)
+
+
+            new_rations << ration.merge(daily_menu).merge(dish).merge(dish_children)
           end
           grouped_rations = new_rations.group_by {|elem| elem[:day_number]}
 
