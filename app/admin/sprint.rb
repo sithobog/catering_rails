043ee7c3fa41d8a41ab48ff8ddaf4fe1c5718f89
Istomin Bohdan <sithobog@ymail.com>
2015-12-02 14:15:28 +0200

    add progress bar for sprint and now admin can view users that ordered/not ordered food for sprint

--- app/admin/sprint.rb
+++ app/admin/sprint.rb
@@ -2,6 +2,46 @@ ActiveAdmin.register Sprint do
 
   permit_params :title, :started_at, :finished_at, :state
 
+  show do
+    all_users = User.all.count.to_f
+    user_with_order = User.with_rations(sprint.id).count.to_f
+    percent = ((user_with_order/all_users)*100).round(1)
+    h2 class: "bar-title" do "Completed orders in sprint" end
+    div class: "progress progress-striped active" do
+        span class: "percent" do
+          "#{percent}%"
+        end
+      div class: "progress-bar", style: "width: #{percent}%;" do
+      end
+    end
+    default_main_content
+  end
+
+  # Show users who have rations in sprint
+  sidebar 'Already ordered ', only: :show do
+    table_for User.with_rations(sprint.id) do
+      column 'Name' do |user|
+        link_to user.name, admin_user_path(user)
+      end
+      column 'Email' do |user|
+        link_to user.email, admin_user_path(user)
+      end
+    end
+  end
+
+  # Show all users who dont have rations in sprint
+  sidebar "Not ordered yet", only: :show do
+    table_for User.without_rations(sprint.id) do
+      column 'Name' do |user|
+        link_to user.name, admin_user_path(user)
+      end
+      column 'Email' do |user|
+        link_to user.email, admin_user_path(user)
+      end
+    end
+  end
+
+
   form do |f|
     f.inputs "Daily Ration" do
       f.input :title
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+
+  has_many :daily_rations
+
+  scope :with_rations, -> (sprint_id) { find(find_rations(sprint_id)) }
+  scope :without_rations, -> (sprint_id) { where.not(id: find_rations(sprint_id)) }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,7 +16,6 @@ class User < ActiveRecord::Base
     self.authentication_token ||= generate_authentication_token
   end
 
-  
   def reset_authentication_token
     self.authentication_token = nil
     self.save
@@ -23,6 +28,16 @@ class User < ActiveRecord::Base
       token = Devise.friendly_token
       break token unless User.where(authentication_token: token).first
     end
+  end
+
+  #find users with rations for specific sprint
+  def self.find_rations(sprint_id)
+    rations = DailyRation.includes(:user).where(sprint_id: sprint_id)
+    users = []
+    rations.each do |ration|
+      users << ration.user.id
+    end
+    users = users.uniq
   end
 
 
